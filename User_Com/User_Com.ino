bc6ba0859091318338c5dda22d8ba5bf61df5e84
Kyle <khallman97@hotmail.com>
2021-01-20 19:34:57 -0500

    Adding user interface code

---
+++ User_Com/User_Com.ino
@@ -0,0 +1,26 @@
+//Decale constants  
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readCan();
+
+  
+}
+
+
+
+void readCan() {
+  //setSoh, setSoc
+  
+}
+
+void setSOH() {
+  
+} 
+///...
+
