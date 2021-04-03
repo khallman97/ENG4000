869742a5c401dd78d5bc03c5b2147fe70157ed50
Kyle Hallman <khallman@my.yorku.ca>
2021-04-02 20:48:56 -0400

    updated code for ui

--- newreadcanWithCodesCompressedAndLEDS/newreadcanWithCodesCompressedAndLEDS.ino
+++ newreadcanWithCodesCompressedAndLEDS/newreadcanWithCodesCompressedAndLEDS.ino
@@ -1,5 +1,6 @@
 #include <ESP32CAN.h>
 #include <CAN_config.h>
+#include <string.h>
 
 CAN_device_t CAN_cfg;
 #define LEDR 25 // RED pin of rgb led is connected to 25 gpio pin  
@@ -46,19 +47,45 @@ CAN_device_t CAN_cfg;
 #define pwm_resolution 8 // 8 bit resolution  
 
 #define buttonPin 15
-int blue = 35; //charged
+int blue = 34; //charged
 int yellow = 33; //charging
 int red = 32; //alarm
 
-
-
-
-
-int soc;
-int currentLED;
-int supplyLED;
+int SOC;
+int currentLED = 0;
+int supplyLED = 0;
 int powerSupplyUnder;
 int powerSupplyOver;
+int switcher = 0;
+int SOH = 100;
+int charging = 0;
+int supplyVoltage = 0;
+
+
+
+//Alarms
+int alarmOn = 0;
+
+int BatteryCellOverTemperatureG = 0;
+int BatteryCellUnderTemperatureG = 0;
+int BatteryCellOverVoltageG = 0;
+int BatteryCellUnderVoltageG = 0;
+int BatteryPackUnderVoltageG = 0;
+int BatteryPackOverVoltageG = 0;
+int FastChargingCurrentG = 0;
+int SlowChargingCurrentG = 0;
+
+int BatteryCellUnderTemperatureS = 0;
+int BatteryCellOverTemperatureS = 0;
+int BatteryCellOverVoltageS = 0;
+int BatteryCellUnderVoltageS = 0;
+int BatteryPackUnderVoltageS = 0;
+int BatteryPackOverVoltageS = 0;
+int FastChargingCurrentS = 0;
+int SlowChargingCurrentS = 0;
+
+int firstCheck = 0;
+int secondCheck =0;
 
 
 void setup() {
@@ -79,16 +106,16 @@ void loop() {
   }
 
   buttonPress();
-
+  printCharge();
 }
 
 void buttonPress() {
 
   //
-  if (switcher == 0) {
+  if (switcher == 0 && alarmOn == 0) {
     printSOC();
   }
-  else if (switcher == 1) {
+  else if (switcher == 1 && alarmOn == 0) {
     printSOH();
   }
   else {
@@ -114,48 +141,202 @@ void readCAN() {
       printf(" RTR from 0x%08x, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
     else {
       //might need to change byte orders around
+      //  printf("frmaeid %x\n",rx_frame.MsgID);
       if (rx_frame.MsgID == 0x1835F4E8) {
-        printf("SOC\n");
-        if (rx_frame.data.u8[0] == 2) {
-          printf("%d \n", rx_frame.data.u8[2]);
-          //Assign SOC value
-          soc = rx_frame.data.u8[2];
-
+        //printf("SOC\n");
+        if (rx_frame.data.u8[0] == 1) {
           //Assign current Value
-          unsigned int precur = (rx_frame.data.u8[3] << 8) | rx_frame.data.u8[4];
+          unsigned int precur = (rx_frame.data.u8[4] << 8) | rx_frame.data.u8[5];
           int current = (precur / 10) - 1600;
           if (current > 2) {
             currentLED = 1;
           }
+          //printf("current raw hex = %x \n", precur);
+          //  printf("current = %d \n", currentLED);
+        }
+        else if (rx_frame.data.u8[0] == 2) {
+          //printf("%d \n", rx_frame.data.u8[2]);
+          //Assign SOC value
+          SOC = rx_frame.data.u8[3];
+          //  printf("SOC = %d \n", SOC);
+
+
 
           //Assign supply voltage
-          unsigned int prevol = (rx_frame.data.u8[5] << 8) | rx_frame.data.u8[6];
-          if (prevol > 2) {
+          unsigned int prevol = (rx_frame.data.u8[6] << 8) | rx_frame.data.u8[7];
+          int supplyVOL = (int)prevol / 10;
+          if (supplyVOL > 2) {
             supplyLED = 1;
           }
+          // printf("current = %d \n", supplyLED);
+          //  printf(" prevol = %d \n", prevol);
           //Add  di2
         }
-        size_t n = sizeof(rx_frame.data.u8) / sizeof(rx_frame.data.u8[0]);
-        for (int i = 0; i < n; i++) {
-
-          printf("%d\t", (int) rx_frame.data.u8[i]);
-        }
-        printf("\n");
+        //        size_t n = sizeof(rx_frame.data.u8) / sizeof(rx_frame.data.u8[0]);
+        //        for (int i = 0; i < n; i++) {
+        //
+        //          printf("%x\t", (int) rx_frame.data.u8[i]);
+        //        }
+        //        printf("\n");
       }
       //Alarms
       else if (rx_frame.MsgID == 0x1838F4E8) {
+        size_t n = sizeof(rx_frame.data.u8) / sizeof(rx_frame.data.u8[0]);
+        for (int i = 0; i < n; i++) {
+
+          printf("%x\t",  rx_frame.data.u8[i]);
+        }
+        printf("\n");
         //byte 0 = 3 general alarms
         if (rx_frame.data.u8[0] == 2) {
-          //power supply under
-          if (rx_frame.data.u8[1] == 1) {
-            powerSupplyUnder = 1;
+          printf("Here value for 1 is %x\n", rx_frame.data.u8[1]);
+          //battery cell over temp
+          if (rx_frame.data.u8[1] == 2 && rx_frame.data.u8[2] == 2 && rx_frame.data.u8[3] == 2) {
+            BatteryCellOverTemperatureG = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellOverTemperatureG = 0;
           }
-          //power supply over
-          else if (rx_frame.data.u8[1] == 2) {
-            powerSupplyOver = 1;
+          //Battery cell under temp general
+          if (rx_frame.data.u8[1] == 2 && rx_frame.data.u8[2] == 4 && rx_frame.data.u8[3] == 4) {
+            BatteryCellUnderTemperatureG = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellUnderTemperatureG = 0;
           }
-          //Add other alarms
+          //Battery Cell over voltage general
+          if (rx_frame.data.u8[1] == 0x10 && rx_frame.data.u8[2] == 0x10 ) {
+            printf("battery cell over\n");
+            BatteryCellOverVoltageG = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellOverVoltageG = 0;
+          }
+          //Battery Cell under voltage general
+          if (rx_frame.data.u8[1] == 0x20 && rx_frame.data.u8[2] == 0x20 ) {
+            BatteryCellUnderVoltageG = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellUnderVoltageG = 0;
+          }
+          
+          //Battery Pack over voltage general
+          if(rx_frame.data.u8[1] == 0x10 ||rx_frame.data.u8[2] == 0 ) {
+            BatteryPackOverVoltageG = 1;
+            
+            alarmOn = 1;
+          } 
+          else {
+            BatteryPackOverVoltageG = 0;
+          }
+          //Battery Pack under voltage general
+          if (rx_frame.data.u8[1] == 8  ) {
+            BatteryPackUnderVoltageG = 1;
+            alarmOn = 1;
+          } else {
+            BatteryPackUnderVoltageG = 0;
+          }
+          //Slow charging current
+          if (rx_frame.data.u8[1] == 0x40  ) {
+            SlowChargingCurrentG = 1;
+            alarmOn = 1;
+          } else {
+            SlowChargingCurrentG = 0;
+          }
+          //Fast charging current
+          if (rx_frame.data.u8[1] == 1 &&  rx_frame.data.u8[2] == 1) {
+            FastChargingCurrentG = 1;
+            alarmOn = 1;
+          } else {
+            FastChargingCurrentG = 0;
+          }
 
+          if (rx_frame.data.u8[1] == 0) {
+            
+            firstCheck = 0;
+          } else {
+            firstCheck = 1;
+          }
+        }
+        //Critical Alarms
+        else if (rx_frame.data.u8[0] == 1) {
+          if (rx_frame.data.u8[1] == 2 && rx_frame.data.u8[2] == 2 && rx_frame.data.u8[3] == 2) {
+            BatteryCellOverTemperatureS = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellOverTemperatureS = 0;
+          }
+          //Battery cell under temp general
+          if (rx_frame.data.u8[1] == 2 && rx_frame.data.u8[2] == 4 && rx_frame.data.u8[3] == 4) {
+            BatteryCellUnderTemperatureS = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellUnderTemperatureS = 0;
+          }
+          //Battery Cell over voltage general
+          if (rx_frame.data.u8[1] == 0x10 && rx_frame.data.u8[2] == 0x10) {
+            BatteryCellOverVoltageS = 1;
+            printf("battery pack over\n");
+            alarmOn = 1;
+          } else {
+            BatteryCellOverVoltageS = 0;
+          }
+          //Battery Cell under voltage general
+          if (rx_frame.data.u8[1] == 0x20 && rx_frame.data.u8[2] == 0x20) {
+            BatteryCellUnderVoltageS = 1;
+            alarmOn = 1;
+          } else {
+            BatteryCellUnderVoltageS = 0;
+          }
+          //Battery Pack over voltage general
+          if (rx_frame.data.u8[1] == 0x10 && rx_frame.data.u8[2] != 0x10 ) {
+            BatteryPackOverVoltageS = 1;
+            
+            alarmOn = 1;
+          } else {
+            BatteryPackOverVoltageS = 0;
+          }
+          //Battery Pack under voltage general
+          if (rx_frame.data.u8[1] == 8) {
+            BatteryPackUnderVoltageS = 1;
+            alarmOn = 1;
+          } else {
+            BatteryPackUnderVoltageS = 0;
+          }
+          //Slow charging current
+          if (rx_frame.data.u8[1] == 0x40) {
+            SlowChargingCurrentS = 1;
+            alarmOn = 1;
+          } else {
+            SlowChargingCurrentS = 0;
+          }
+          //Fast charging current
+          if (rx_frame.data.u8[1] == 1 &&  rx_frame.data.u8[2] == 1) {
+            FastChargingCurrentS = 1;
+            alarmOn = 1;
+          } else {
+            FastChargingCurrentS = 0;
+          }
+
+          if (rx_frame.data.u8[1] == 0) {
+            
+            secondCheck = 0;
+          } else {
+            secondCheck = 1;
+          }
+        }
+        if(firstCheck == 0 && secondCheck ==0) {
+          alarmOn = 0;
+          digitalWrite(red, LOW);
+        }
+
+        
+      }
+      else if (rx_frame.MsgID == 0x1834F4E8) {
+        //printf("SOH stuff\n");
+        if (rx_frame.data.u8[0] == 1) {
+          SOH = rx_frame.data.u8[2];
+          printf("SOH = %d \n", SOH);
         }
       }
     }
@@ -166,49 +347,57 @@ void readCAN() {
 void printSOC() {
   //int SOC = 100;
   if (SOC <= 10 ) {
+
     RGB_Right2(255, 255, 0); // Yellow
     RGB_Right1(255, 0, 0); // Red
     RGB_Left2(255, 0, 0); // RED ccolor
     RGB_Left1(255, 0, 0); // RED ccolor
+    RGB_Right3(255, 0, 0); // green
   }
   else if (SOC > 10 && SOC <= 20) {
+
     RGB_Right2(0, 255, 0); // green
     RGB_Right1(255, 0, 0); // Red
     RGB_Left2(255, 0, 0); // RED ccolor
     RGB_Left1(255, 0, 0); // RED ccolor
+    RGB_Right3(255, 0, 0); // green
   }
-  else if (SOC > 20 && SOC <= 50) {
+  else if (SOC > 20 && SOC <= 40) {
+    RGB_Right3(0, 255, 0); // green
     RGB_Right2(0, 255, 0); // green
     RGB_Right1(0, 255, 0); // green
     RGB_Left2(255, 0, 0); // RED ccolor
     RGB_Left1(255, 0, 0); // RED ccolor
   }
-  else if (SOC > 50 && SOC <= 80) {
+  else if (SOC > 40 && SOC <= 60) {
+    RGB_Right3(0, 255, 0); // green
     RGB_Right2(0, 255, 0); // green
-    RGB_Right1(0, 255, 0); // green
-    RGB_Left2(0, 255, 0); // green
+    RGB_Right1(255, 255, 0); // green
+    RGB_Left2(255, 0, 0); // green
     RGB_Left1(255, 0, 0); // RED ccolor
   }
-  else if (SOC > 80 && SOC <= 90) {
-    
+  else if (SOC > 60 && SOC <= 80) {
+
     RGB_Right2(0, 255, 0); // green
     RGB_Right1(0, 255, 0); // green
     RGB_Left2(0, 255, 0); // green
-    RGB_Left1(255, 255, 0); // yellow
-  }
-  else if (SOC > 90) {
+    RGB_Left1(0, 255, 0); // yellow
     RGB_Right3(0, 255, 0); // green
+  }
+  else if (SOC > 80) {
+
     RGB_Right2(0, 255, 0); // green
     RGB_Right1(0, 255, 0); // green
     RGB_Left2(0, 255, 0); // green
     RGB_Left1(0, 255, 0); // green
+    RGB_Right3(0, 255, 0); // green
   }
 }
 
 
 void printSOH() {
 
-  if(SOH < 3){
+  if (SOH < 3) {
     RGB_Right3(255, 255, 0); // Yellow
     RGB_Right2(255, 0, 0); // Red
     RGB_Right1(255, 0, 0); // Red
@@ -254,77 +443,140 @@ void printSOH() {
 
 void printCharge() {
   int batterylevel = 100;
-  if (supplyVoltage == 1) {
+  if (supplyLED == 1) {
     digitalWrite(blue, HIGH); // turn the LED on by making the voltage HIGH
-    if (charging == 1) {
+    if (currentLED == 1) {
       digitalWrite(yellow, HIGH); // turn the LED off by making the voltage LOW
     }
   }
 }
 
 void printAlarm() {
-  int alarm = 0;
-  if (alarm == 0) {
+  if (alarmOn == 0) {
     //no alarm means no change in UI
     digitalWrite(red, LOW);
   }
   else {
-    digitalWrite(red, HIGH); // turn the LED on by making the voltage HIGH
-    if (alarm == 1) {
+     // turn the LED on by making the voltage HIGH
+    if (BatteryCellOverTemperatureG  == 1) {
       RGB_Left1(255, 255, 0); // yellow ccolor
       RGB_Left2(255, 255, 0); // yellow ccolor
       RGB_Right3(255, 255, 0); // yellow ccolor
       RGB_Right1(255, 255, 0); // yellow ccolor
       RGB_Right2(255, 255, 0); // yellow ccolor
     }
-    else if (alarm == 2) {
+    if (BatteryCellUnderTemperatureG  == 1) {
       RGB_Left1(255, 0, 0); // RED ccolor
       RGB_Left2(255, 0, 0); // RED ccolor
       RGB_Right3(255, 0, 0); // RED ccolor
       RGB_Right1(255, 0, 0); // RED ccolor
       RGB_Right2(255, 0, 0); // RED ccolor
     }
-    else if (alarm == 3) {
-      RGB_Left1(0, 255, 0); // green ccolor
-      RGB_Left2(0, 255, 0); // green ccolor
-      RGB_Right3(0, 255, 0); // green ccolor
-      RGB_Right1(0, 255, 0); // green ccolor
-      RGB_Right2(0, 255, 0); // green ccolor
+    if (BatteryCellOverVoltageG  == 1) {
+      RGB_Left1(164, 62, 233); // green ccolor
+      RGB_Left2(164, 62, 233); // green ccolor
+      RGB_Right3(164, 62, 233); // green ccolor
+      RGB_Right1(164, 62, 233); // green ccolor
+      RGB_Right2(164, 62, 233); // green ccolor
     }
-    else if (alarm == 4) {
+    if (BatteryCellUnderVoltageG  == 1) {
       RGB_Left1(255, 192, 203); // pink ccolor
       RGB_Left2(255, 192, 203); // pink ccolor
       RGB_Right3(255, 192, 203); // pink ccolor
       RGB_Right1(255, 192, 203); // pink ccolor
       RGB_Right2(255, 192, 203); // pink ccolor
     }
-    else if (alarm == 5) {
-      RGB_Left1(255, 69, 0); // orange ccolor
+    if (BatteryPackUnderVoltageG  == 1) {
+      RGB_Left1(55, 55, 0); // orange ccolor
       RGB_Left2(255, 69, 0); // orange ccolor
       RGB_Right3(255, 69, 0); // orange ccolor
       RGB_Right1(255, 69, 0); // orange ccolor
       RGB_Right2(255, 69, 0); // orange ccolor
     }
-    else if (alarm == 6) {
-      RGB_Left1(0, 0, 255); // blue ccolor
-      RGB_Left2(0, 0, 255); // blue ccolor
-      RGB_Right3(0, 0, 255); // blue ccolor
-      RGB_Right1(0, 0, 255); // blue ccolor
-      RGB_Right2(0, 0, 255); // blue ccolor
+    if (FastChargingCurrentG  == 1) {
+      RGB_Left1(0, 128, 255); // blue ccolor
+      RGB_Left2(0, 28, 255); // blue ccolor
+      RGB_Right3(255, 255, 255); // blue ccolor
+      RGB_Right1(255, 0, 255); // blue ccolor
+      RGB_Right2(255, 255, 255); // blue ccolor
     }
-    else if (alarm == 7) {
-      RGB_Left1(255, 255, 255); // white ccolor
-      RGB_Left2(255, 255, 255); // white ccolor
+    if (SlowChargingCurrentG  == 1) {
+      RGB_Left1(24, 255, 67); // white ccolor
+      RGB_Left2(84, 78, 2); // white ccolor
       RGB_Right3(255, 255, 255); // white ccolor
       RGB_Right1(255, 255, 255); // white ccolor
       RGB_Right2(255, 255, 255); // white ccolor
     }
-    else if (alarm == 8) {
-      RGB_Left1(153, 0, 153); // purple ccolor
-      RGB_Left2(153, 0, 153); // purple ccolor
-      RGB_Right3(153, 0, 153); // purple ccolor
-      RGB_Right1(153, 0, 153); // purple ccolor
-      RGB_Right2(153, 0, 153); // purple ccolor
+    if (BatteryPackOverVoltageG  == 1) {
+      RGB_Left1(50, 80, 155); // white ccolor
+      RGB_Left2(50, 80, 155); // white ccolor
+      RGB_Right3(50, 80, 155); // white ccolor
+      RGB_Right1(50, 80, 155); // white ccolor
+      RGB_Right2(50, 80, 155); // white ccolor
+    }
+    if (BatteryCellUnderTemperatureS  == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(164, 62, 233); // purple ccolor
+      RGB_Left2(164, 62, 233); // purple ccolor
+      RGB_Right3(164, 62, 233); // purple ccolor
+      RGB_Right1(164, 62, 233); // purple ccolor
+      RGB_Right2(164, 62, 233); // purple ccolor
+    }
+    if (BatteryCellOverVoltageS    == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 0, 0); // RED ccolor
+      RGB_Left2(255, 0, 0); // RED ccolor
+      RGB_Right3(255, 0, 0); // RED ccolor
+      RGB_Right1(255, 0, 0); // RED ccolor
+      RGB_Right2(0, 0, 255); // RED ccolor
+    }
+    if (BatteryCellUnderVoltageS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 192, 203); // pink ccolor
+      RGB_Left2(255, 192, 203); // pink ccolor
+      RGB_Right3(255, 192, 203); // pink ccolor
+      RGB_Right1(255, 192, 203); // pink ccolor
+      RGB_Right2(255, 192, 203); // pink ccolor
+    }
+    if (BatteryPackUnderVoltageS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 192, 203); // pink ccolor
+      RGB_Left2(255, 192, 203); // pink ccolor
+      RGB_Right3(255, 192, 203); // pink ccolor
+      RGB_Right1(255, 192, 203); // pink ccolor
+      RGB_Right2(255, 0, 0); // pink ccolor
+    }
+    if (BatteryPackOverVoltageS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 69, 0); // orange ccolor
+      RGB_Left2(255, 69, 0); // orange ccolor
+      RGB_Right3(255, 69, 0); // orange ccolor
+      RGB_Right1(255, 69, 0); // orange ccolor
+      RGB_Right2(255, 0, 0); // orange ccolor
+    }
+    if (FastChargingCurrentS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(76, 155, 255); // blue ccolor
+      RGB_Left2(188, 255, 89); // blue ccolor
+      RGB_Right3(0, 0, 255); // blue ccolor
+      RGB_Right1(0, 0, 255); // blue ccolor
+      RGB_Right2(255, 0, 0); // blue ccolor
+    }
+    if (SlowChargingCurrentS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 255, 255); // white ccolor
+      RGB_Left2(255, 255, 255); // white ccolor
+      RGB_Right3(255, 255, 255); // white ccolor
+      RGB_Right1(255, 255, 255); // white ccolor
+      RGB_Right2(255, 0, 0); // white ccolor
+    }
+    if (BatteryCellOverTemperatureS   == 1) {
+      digitalWrite(red, HIGH);
+      RGB_Left1(255, 255, 255); // white ccolor
+      RGB_Left2(255, 255, 255); // white ccolor
+      RGB_Right3(255, 255, 255); // white ccolor
+      RGB_Right1(255, 0, 0); // white ccolor
+      RGB_Right2(255, 0, 0); // white ccolor
     }
   }
 }
@@ -334,6 +586,9 @@ void initLEDS() {
   pinMode(blue, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(buttonPin, INPUT);
+  //  digitalWrite(red, HIGH);
+  //  digitalWrite(blue, HIGH);
+  //  digitalWrite(yellow, HIGH);
 
   //RGB 1
   ledcAttachPin(LEDR, R_channel);
